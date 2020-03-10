ed00fecb3acd5f99c9ce87bf55140dce3cd63df4
liuli <ll382640782@126.com>
2020-03-10 10:23:32 +0800

    add

--- main_gui.py
+++ main_gui.py
@@ -1,36 +1,80 @@
-import sys
-from PyQt5.QtWidgets import QMainWindow, QAction, qApp, QApplication
-from PyQt5.QtGui import QIcon
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'main_gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
 
 
-class Example(QMainWindow):
-
-    def __init__(self):
-        super().__init__()
-
-        self.initUI()
-
-    def initUI(self):
-        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
-        exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Exit application')
-        exitAction.triggered.connect(qApp.quit)
-
-        self.statusBar()
-
-        # 创建一个菜单栏
-        menubar = self.menuBar()
-        # 添加菜单
-        fileMenu = menubar.addMenu('&File')
-        # 添加事件
-        fileMenu.addAction(exitAction)
-
-        self.setGeometry(300, 300, 300, 200)
-        self.setWindowTitle('Menubar')
-        self.show()
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = Example()
-    sys.exit(app.exec_())
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1122, 900)
+        MainWindow.setWindowTitle("")
+        MainWindow.setDocumentMode(True)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 250, 113, 32))
+        self.pushButton.setObjectName("pushButton")
+        self.dateTimeEdit = QtWidgets.QDateTimeEdit(self.centralwidget)
+        self.dateTimeEdit.setGeometry(QtCore.QRect(10, 0, 121, 24))
+        self.dateTimeEdit.setObjectName("dateTimeEdit")
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(130, 0, 71, 51))
+        self.pushButton_2.setObjectName("pushButton_2")
+        self.toolButton = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton.setGeometry(QtCore.QRect(200, 210, 37, 18))
+        self.toolButton.setObjectName("toolButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menuBar = QtWidgets.QMenuBar(MainWindow)
+        self.menuBar.setGeometry(QtCore.QRect(0, 0, 1122, 23))
+        self.menuBar.setAutoFillBackground(False)
+        self.menuBar.setDefaultUp(True)
+        self.menuBar.setObjectName("menuBar")
+        self.menu = QtWidgets.QMenu(self.menuBar)
+        self.menu.setTearOffEnabled(True)
+        self.menu.setObjectName("menu")
+        self.menu_2 = QtWidgets.QMenu(self.menuBar)
+        self.menu_2.setObjectName("menu_2")
+        MainWindow.setMenuBar(self.menuBar)
+        self.toolBar = QtWidgets.QToolBar(MainWindow)
+        self.toolBar.setObjectName("toolBar")
+        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.toolBar)
+        self.action_3 = QtWidgets.QAction(MainWindow)
+        self.action_3.setObjectName("action_3")
+        self.actionddd = QtWidgets.QAction(MainWindow)
+        self.actionddd.setObjectName("actionddd")
+        self.actionaa = QtWidgets.QAction(MainWindow)
+        self.actionaa.setObjectName("actionaa")
+        self.actionjj = QtWidgets.QAction(MainWindow)
+        self.actionjj.setObjectName("actionjj")
+        self.menu.addAction(self.actionjj)
+        self.menu.addAction(self.actionddd)
+        self.menu.addAction(self.actionaa)
+        self.menu.addSeparator()
+        self.menuBar.addAction(self.menu.menuAction())
+        self.menuBar.addAction(self.menu_2.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        self.pushButton.setText(_translate("MainWindow", "PushButton"))
+        self.pushButton_2.setText(_translate("MainWindow", "确认"))
+        self.toolButton.setText(_translate("MainWindow", "..."))
+        self.menu.setTitle(_translate("MainWindow", "文件"))
+        self.menu_2.setTitle(_translate("MainWindow", "编辑"))
+        self.toolBar.setWindowTitle(_translate("MainWindow", "toolBar"))
+        self.action_3.setText(_translate("MainWindow", "新建"))
+        self.actionddd.setText(_translate("MainWindow", "打开"))
+        self.actionaa.setText(_translate("MainWindow", "保存"))
+        self.actionjj.setText(_translate("MainWindow", "新建"))
