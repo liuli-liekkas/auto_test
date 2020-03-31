bb5f3c063157608fe5195240cf025aef85f01c6d
liuli <ll382640782@126.com>
2020-04-01 07:17:21 +0800

    Revert "add"
    This reverts commit 68ed75bb

--- Database.py
+++ Database.py
@@ -1,5 +1,18 @@
 import sys
 from PyQt5.QtSql import QSqlDatabase
+from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 
-class Database()
+class Database(QWidget):
+    def __init__(self):
+        self.db = None
+        self.db_connect()
+
+    def db_connect(self):
+        self.db = QSqlDatabase.addDatabase('QMYSQL')
+        self.db.setHostName('localhost')
+        self.db.setDatabaseName('test_db')
+        self.db.setUserName('root')
+        self.db.setPassword('ll891119')
+        if not self.db.open():
+
