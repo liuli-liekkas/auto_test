0424f0a4f3f9c3082c99d9802add28632db3e865
liuli <ll382640782@126.com>
2020-04-01 18:07:53 +0800

    add

--- Database.py
+++ Database.py
@@ -4,15 +4,26 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 
 
 class Database(QWidget):
-    def __init__(self):
-        self.db = None
-        self.db_connect()
+	def __init__(self):
+		super(Database, self).__init__()
+		self.db = None
+		self.db_connect()
 
-    def db_connect(self):
-        self.db = QSqlDatabase.addDatabase('QMYSQL')
-        self.db.setHostName('localhost')
-        self.db.setDatabaseName('test_db')
-        self.db.setUserName('root')
-        self.db.setPassword('ll891119')
-        if not self.db.open():
+	def db_connect(self):
+		self.db = QSqlDatabase.addDatabase('QMYSQL')
+		self.db.setHostName('localhost')
+		self.db.setDatabaseName('test')
+		self.db.setUserName('root')
+		self.db.setPassword('ll891119')
+		if not self.db.open():
+			QMessageBox.critical(self, 'Database Connection', self.db.lastError().text())
 
+	def closeEvent(self, QCloseEvent):
+		self.db.close()
+
+
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	database = Database()
+	database.show()
+	sys.exit(app.exec_())
--- normal_test.py
+++ normal_test.py
@@ -1,26 +1,12 @@
 import sys
-from PyQt5.QtSql import QSqlDatabase
-from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
-
-
-class Demo(QWidget):
-    def __init__(self):
-        super(Demo, self).__init__()
-        self.db = None
-        self.db_connect()
-
-    def db_connect(self):
-        self.db = QSqlDatabase.addDatabase('QSQLITE')    # 1
-        self.db.setDatabaseName('./test.db')             # 2
-        if not self.db.open():                           # 3
-            QMessageBox.critical(self, 'Database Connection', self.db.lastError().text())
-
-    def closeEvent(self, QCloseEvent):                   # 4
-        self.db.close()
-
-
+from PyQt5 import QtSql
+import ctypes
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    demo = Demo()
-    demo.show()
-    sys.exit(app.exec_())
+	db = QtSql.QSqlDatabase.addDatabase("QMYSQL")
+	db.setHostName("localhost")
+	db.setDatabaseName("mysql")
+	db.setUserName("root")
+	db.setPassword("ll891119")
+	db.setPort(3306)
+	db.open()
+	print(QtSql.QSqlDatabase.drivers())
