dfc3b3047acb605b651d359ded773056943c2d2c
liuli <ll382640782@126.com>
2020-04-10 17:11:18 +0800

    add

--- CanAnalysis.py
+++ CanAnalysis.py
@@ -1,5 +1,7 @@
 import ctypes
 from ctypes import wintypes
+from cantools import database
+import cantools
 
 Struct_USBCAN2C = 4
 STATUS_OK = 1
@@ -30,60 +32,72 @@ class Struct_CAN_OBJ(ctypes.Structure):
 
 
 if __name__ == '__main__':
-    # CanDLLName = './dll/D4ARTS6x64.dll'
-    # CanDLLName = './dll/PcommDllx64.dll'
-    # CanDLLName = './dll/CHUSBDLL64.dll'
+    db = database.load_file('./dbc/ARS408_can_database.dbc')
+    # print(db.messages)
     CanDLLName = './dll/ECanVci64.dll'
     canDLL = ctypes.CDLL(CanDLLName)
     ret = canDLL.OpenDevice(Struct_USBCAN2C, 0, 0)
-    print(ret, '1')
     if ret != STATUS_OK:
         print('调用 Struct_OpenDevice出错\r\n')
     # 初始0通道
     struct_initconfig = Struct_INIT_CONFIG(0, 0xFFFFFFFF, 0, 0, 0, 0x1C, 0)
     ret = canDLL.InitCAN(Struct_USBCAN2C, 0, 0, ctypes.byref(struct_initconfig))
-    print(ret, '2')
     if ret != STATUS_OK:
         print('调用 Struct_InitCAN出错\r\n')
 
     ret = canDLL.StartCAN(Struct_USBCAN2C, 0, 0)
-    print(ret, '3')
     if ret != STATUS_OK:
         print('调用 Struct_StartCAN出错\r\n')
 
     # 初始1通道
     ret = canDLL.InitCAN(Struct_USBCAN2C, 0, 1, ctypes.byref(struct_initconfig))
-    print(ret, '4')
     if ret != STATUS_OK:
         print('调用 Struct_InitCAN 1 出错\r\n')
-
     ret = canDLL.StartCAN(Struct_USBCAN2C, 0, 1)
-    print(ret, '5')
     if ret != STATUS_OK:
         print('调用 Struct_StartCAN 1 出错\r\n')
 
     # 通道0发送数据
+    # ubyte_array = ctypes.c_ubyte * 8
+    # a = ubyte_array(1, 2, 3, 4, 5, 6, 7, 64)
+    # ubyte_3array = ctypes.c_ubyte * 3
+    # b = ubyte_3array(0, 0, 0)
+    # struct_can_obj = Struct_CAN_OBJ(0x0, 0, 0, 1, 0, 0, 8, a, b)
+    # ret = canDLL.Transmit(Struct_USBCAN2C, 0, 0, ctypes.byref(struct_can_obj), 1)
+    # print(ret, '6')
+    # if ret != STATUS_OK:
+    #     print('调用 Struct_Transmit 出错\r\n')
+
+    # 通道0接收数据
     ubyte_array = ctypes.c_ubyte * 8
-    a = ubyte_array(1, 2, 3, 4, 5, 6, 7, 64)
+    a = ubyte_array(0, 0, 0, 0, 0, 0, 0, 1)
     ubyte_3array = ctypes.c_ubyte * 3
     b = ubyte_3array(0, 0, 0)
-    struct_can_obj = Struct_CAN_OBJ(0x0, 0, 0, 1, 0, 0, 8, a, b)
-    ret = canDLL.Transmit(Struct_USBCAN2C, 0, 0, ctypes.byref(struct_can_obj), 1)
-    print(ret, '6')
-    if ret != STATUS_OK:
-        print('调用 Struct_Transmit 出错\r\n')
+    struct_can_obj = Struct_CAN_OBJ(0x0, 1, 1, 0, 0, 0, 8, a, b)
+    while True:
+        # print('调用 Struct_Receive 出错\r\n')
+        ret = canDLL.Receive(Struct_USBCAN2C, 0, 0, ctypes.byref(struct_can_obj), 1, 10)
+        if ret > 0:
+            if struct_can_obj.ID == 1547:
+                # print(struct_can_obj.ID)
+                # print(struct_can_obj.DataLen)
+                struct_can_obj_data = list(struct_can_obj.Data)
+                for i in range(len(struct_can_obj_data)):
+                    print(bin(int(struct_can_obj_data[i], 10)))
+                    # struct_can_obj_data = [hex(i) for i in struct_can_obj_data]
+                    # print(struct_can_obj_data)
+
 
     # 通道1接收数据
-    a = ubyte_array(0, 0, 0, 0, 0, 0, 0, 1)
-    struct_can_obj = Struct_CAN_OBJ(0x0, 0, 0, 1, 0, 0, 8, a, b)
-    ret = canDLL.Receive(Struct_USBCAN2C, 0, 1, ctypes.byref(struct_can_obj), 1, 110)
-    print(ret, '7')
-    while ret <= 0:
-        print('调用 Struct_Receive 出错\r\n')
-        ret = canDLL.Receive(Struct_USBCAN2C, 0, 1, ctypes.byref(struct_can_obj), 1, 110)
-    if ret > 0:
-        print(struct_can_obj.DataLen)
-        print(list(struct_can_obj.Data))
+    # a = ubyte_array(0, 0, 0, 0, 0, 0, 0, 1)
+    # struct_can_obj = Struct_CAN_OBJ(0x0, 0, 0, 1, 0, 0, 8, a, b)
+    # ret = canDLL.Receive(Struct_USBCAN2C, 0, 1, ctypes.byref(struct_can_obj), 1, 50)
+    # while ret <= 0:
+    #     print('调用 Struct_Receive 出错\r\n')
+    #     ret = canDLL.Receive(Struct_USBCAN2C, 0, 1, ctypes.byref(struct_can_obj), 1, 50)
+    # if ret > 0:
+    #     print(struct_can_obj.DataLen)
+    #     print(list(struct_can_obj.Data))
 
     # 关闭
     canDLL.CloseDevice(Struct_USBCAN2C, 0)
--- normal_test.py
+++ normal_test.py
@@ -1,13 +1,109 @@
 import sys
-from PyQt5 import QtSql
-import ctypes
-if __name__ == '__main__':
-	db = QtSql.QSqlDatabase.addDatabase("QMYSQL")
-	db.setHostName("localhost")
-	db.setDatabaseName("mysql")
-	db.setUserName("root")
-	db.setPassword("ll891119")
-	db.setPort(3306)
-	print(db.open())
-	print(QtSql.QSqlDatabase.drivers())
+print("----------------欢迎使用CAN报文转换工具交互模式----------------")
+print("请输入CAN信号,格式为:startBit:length:minValue:maxValue:setValue")
+print("例如：32:1:0:1:1")
+print("或者省略minValue和maxValue：35：1：：：1")
+print("信号输入结束请再按一次回车")
 
+#十进制转换成二进制list
+def octToBin(octNum, bit):
+    while(octNum != 0):
+        bit.append(octNum%2)
+        octNum = int(octNum/2)
+    for i in range(64-len(bit)):
+        bit.append(0)
+
+sig = []
+startBit = []
+length = []
+setValue = []
+#输入CAN信号
+while True:
+    input_str = input()
+    if not len(input_str):
+        break
+    if(input_str.count(":")<4):
+        print("输入格式错误，参数缺少setValue,请重新输入！")
+        continue
+    if(input_str.split(":")[4]==""):
+        print("setValue参数不能为空，请重新输入！")
+        continue
+    sig.append(input_str)
+    print(sig)
+#解析CAN信号
+for i in range(len(sig)):
+    startBit.append(int(sig[i].split(":")[0]))
+    length.append(int(sig[i].split(":")[1]))
+    setValue.append(int(sig[i].split(":")[4]))
+#CAN数组存放CAN报文值
+# 0:8:::165
+# 24:12:::1701
+# 54:5:::25
+print(startBit, length, setValue)
+CAN = []
+for i in range(64):
+    CAN.append(-1)
+for i in range(len(startBit)):
+    #长度超过1Byte的情况，暂不支持
+    if(length[i]>16):
+        print("CAN信号长度超过2Byte，暂不支持！！！")
+        sys.stdin.readline()
+        sys.exit()
+    #长度未超过1Byte的情况且未跨字节的信号
+    if((startBit[i]%8 + length[i])<=8):
+        for j in range(length[i]):
+            bit = []
+            #setValue的二进制值按字节位从低到高填
+            octToBin(setValue[i],bit)
+            print(bit)
+            #填满字节长度值
+            if(CAN[startBit[i]+j]==-1):
+                CAN[startBit[i]+j] = bit[j]
+            #字节存在冲突
+            else:
+                print(sig[i] + "字节位存在冲突，生成CAN报文失败！！！")
+                sys.stdin.readline()
+                sys.exit()
+    #跨字节的信号
+    else:
+        #高位位数和低位位数
+        highLen = 8 - startBit[i]%8
+        lowLen = length[i] - highLen
+        bit = []
+        #setValue的二进制值按字节位从低到高填
+        octToBin(setValue[i],bit)
+        #先填进信号的高位
+        for j1 in range(highLen):
+            if(CAN[startBit[i]+j1]==-1):
+                CAN[startBit[i]+j1] = bit[j1]
+            #字节存在冲突
+            else:
+                print(sig[i] + "字节位存在冲突，生成CAN报文失败！！！")
+                sys.stdin.readline()
+                sys.exit()
+        #再填进信号的低位
+        for j2 in range(lowLen):
+            if(CAN[(int(startBit[i]/8)-1)*8+j2]==-1):
+                CAN[(int(startBit[i]/8)-1)*8+j2] = bit[highLen+j2]
+            #字节存在冲突
+            else:
+                print(sig[i] + "字节位存在冲突，生成CAN报文失败！！！")
+                sys.stdin.readline()
+                sys.exit()
+#剩余位默认值设为0
+for i in range(64):
+    if(CAN[i]==-1):
+        CAN[i] = 0
+#----------------将二进制list每隔8位转换成十六进制输出----------------
+#其中，map()将list中的数字转成字符串，按照Motorola格式每隔8位采用了逆序
+# ''.join()将二进制list转换成二进制字符串，int()将二进制字符串转换成十进制
+#hex()再将十进制转换成十六进制，upper()转换成大写，两个lstrip()将"0X"删除,
+#zfill()填充两位，输出不换行，以空格分隔
+print(hex(int(''.join(map(str,CAN[7::-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[15:7:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[23:15:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[31:23:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[39:31:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[47:39:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[55:47:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2) + " ",end="")
+print(hex(int(''.join(map(str,CAN[63:55:-1])),2)).upper().lstrip("0").lstrip("X").zfill(2))
